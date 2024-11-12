3bda3832f085fa2fa069aa13a2a897d196c7d880
profechino00 <nico.ybanez00@gmail.com>
2024-11-12 15:14:57 -0300

    Add files via upload

---
+++ trabajos/prueba.js
@@ -0,0 +1,13 @@
+try{
+	const.resultado = dividir(10, 0);
+	console.log(resultado);
+} catch (error){
+console.log('Error:' error.message);
+} finally {
+	console.log('Operacion finalizada')
+}
+function dividir(a, b) {
+	if (b===0) {
+		
+	}
+}
